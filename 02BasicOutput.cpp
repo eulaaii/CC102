e82cacc40672343c467d05aca1530210b182414f
EULAJOY LUCES BRAVANTE <luceseula28@gmail.com>
2024-10-05 18:31:56 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Our Lady of Lourdes College Foundation\n";
+	cout << "College of Coputer Studies\n";
+	cout << "Bachelor of Sciience in Information Technology 1\n";
+	cout << "Bravante, Eulajoy L.\n";
+	cout << "Brgy. Vinas Calauag Quezon\n";
+	return 0;
+}
---
+++ 04BasicInput.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string name;
+	string course;
+	string school;
+	
+	cout << "Nickname\t\t:\t";
+	cin >> name;
+	cout << "Course-Year Level\t:\t";
+	cin >> course;
+	cout << "School\t\t\t:\t";
+	cin >> school;
+	return 0;
+}
