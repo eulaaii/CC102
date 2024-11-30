bf3ddc9500471d89b56f07b7e5229b49f052793d
Bravante, Eulajoy L. <luceseula28@gmail.com>
2024-12-01 00:31:02 +0800

    Act_1a.cpp

--- Act_1a.cpp
+++ Act_1a.cpp
@@ -3,9 +3,13 @@ using namespace std;
 
 int main()
 {
-	for (int i = 1; i++;) {
-		for (int x = i; x++;)
-			cout << " # *";
+	for (int h = 1; h <= 5; h++){
+		for (int r = 1; r <= 10; r++)
+			cout << " # *" << symbol2;
+		cout << endl;
+		for (int z = 1; z <= 10; z++)
+			cout << symbol2 << "# * ";
+		cout << endl;
 	}
 	return 0;
 }
